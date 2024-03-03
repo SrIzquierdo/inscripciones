fd765d982d545d2c9d1fa210d09c35f0dc793a6e
SrIzquierdo <146435002+SrIzquierdo@users.noreply.github.com>
2024-03-03 18:21:23 +0100

    Modelo cambiado

--- public/php/model/modelo.php
+++ public/php/model/modelo.php
@@ -3,22 +3,33 @@
     class Modelo extends Conexion{
         public function tabla_inscripciones(){
             $sql = "SELECT alumno.nombre AS alumno, clase.nombre AS clase, actividad.nombre AS actividad
-                FROM alumno
-                JOIN clase ON alumno.id_clase = clase.id
-                LEFT JOIN inscripcion ON inscripcion.id_alumno = alumno.id
-                LEFT JOIN actividad ON inscripcion.id_actividad = actividad.id";
+                    FROM alumno
+                    JOIN clase ON alumno.id_clase = clase.id
+                    LEFT JOIN inscripcion ON inscripcion.id_alumno = alumno.id
+                    LEFT JOIN actividad ON inscripcion.id_actividad = actividad.id
+                    ORDER BY actividad, clase";
             $stmt = $this->conexion->prepare($sql);
             $stmt->execute();
-
+        
             $resultado = $stmt->get_result();
             $datos = array();
             while ($fila = $resultado->fetch_assoc()) {
-                array_push($datos, $fila);
+                $actividad = $fila['actividad'];
+                $clase = $fila['clase'];
+                $alumno = $fila['alumno'];
+        
+                // Agrupar los datos por actividad y luego por clase dentro de cada actividad
+                if (!isset($datos[$actividad])) {
+                    $datos[$actividad] = array();
+                }
+        
+                if (!isset($datos[$actividad][$clase])) {
+                    $datos[$actividad][$clase] = array();
+                }
+        
+                $datos[$actividad][$clase][] = $alumno;
             }
-
-            $stmt->close();
-            $this->conexion->close();
-
+        
             return $datos;
         }
     }
